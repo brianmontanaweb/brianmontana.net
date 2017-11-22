1e41f8af61ab1c194aae8db3e00734a2f3383580
Brian Montana <contact@brianmontana.net>
2017-11-22 14:17:43 -0600

    Update for browsersync and webpack. CSS fixes to avoid using position absolute, not needed in this instance

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,25 @@
 module.exports = {
   entry: './app/js/app.js',
   output: {
-    path: __dirname + '/dist/js',
+    path: __dirname + '/app/js',
     filename: 'bundle.min.js'
   },
   module: {
-    loaders: [
-      {
-        test: /\.(scss|sass)$/,
-        loaders: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(svg|gif|png|eot|woff|ttf)$/,
-        loaders: [
-          'url-loader'
-        ]
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader?presets[]=es2015'
-      }
-    ]
+    rules: [{
+      test: /\.(scss|sass)$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+      }, {
+        loader: 'sass-loader'
+      }]
+    }, {
+      test: /\.(svg|gif|png|eot|woff|ttf)$/,
+      loader: 'url-loader'
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader?presets[]=es2015'
+    }]
   }
 };
